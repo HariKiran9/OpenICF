f9100ab772c71374831a1c63d584c79e12beae79
HariKiran9 <hari.chebrol@gmail.com>
2019-07-11 22:15:14 +0530

    initial commit

---
+++ src/main/java/identity/dbconnector/DataBaseIdentityFilter.java
@@ -0,0 +1,139 @@
+/**
+ * 
+ */
+package identity.dbconnector;
+
+import java.util.Date;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.common.exceptions.ConnectorException;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.filter.AbstractFilterTranslator;
+import org.identityconnectors.framework.common.objects.filter.ContainsFilter;
+import org.identityconnectors.framework.common.objects.filter.EqualsFilter;
+import org.identityconnectors.framework.common.objects.filter.GreaterThanFilter;
+
+import identity.dbconnector.util.DataBaseIdentityUtil;
+
+/**
+ * @author bc887d
+ *
+ */
+public class DataBaseIdentityFilter extends AbstractFilterTranslator<String> {
+	private Log logger = Log.getLog(this.getClass());
+
+	public String createEqualsExpression(EqualsFilter filter, boolean not) {
+		String operation = "createEqualsExpression - ";
+
+		logger.ok(operation + " Started");
+
+		logger.ok(operation + " filter " + filter);
+
+		String query = null;
+		if (not) {
+			return query;
+		}
+
+		Attribute attr = filter.getAttribute();
+		if (attr == null || attr.getValue() == null || (attr.getValue() != null && attr.getValue().isEmpty())) {
+			return query;
+		}
+
+		String name = filter.getName();
+		Object val = attr.getValue().get(0);
+		if (DataBaseIdentityUtil.isEmpty(val)) {
+			throw new ConnectorException("The filter value can not be empty");
+		}
+		String strValue = DataBaseIdentityUtil.getString(val);
+		logger.info(" Ended" + strValue);
+		query = "T." + name + "='" + val + "'";
+		logger.ok(operation + " Final Filter " + query);
+		logger.ok(operation + " Ended");
+
+		return query;
+	}
+
+	@Override
+	protected String createContainsExpression(ContainsFilter filter, boolean not) {
+		String operation = "createContainsExpression - ";
+		logger.ok(operation + " Started");
+		String query = null;
+		if (not) {
+			return query;
+		}
+
+		Attribute attr = filter.getAttribute();
+		if (attr == null || attr.getValue() == null || (attr.getValue() != null && attr.getValue().isEmpty())) {
+			return query;
+		}
+
+		String name = filter.getName();
+		Object val = attr.getValue().get(0);
+		if (DataBaseIdentityUtil.isEmpty(val)) {
+			throw new ConnectorException("The filter value can not be empty");
+		}
+		String strValue = DataBaseIdentityUtil.getString(val);
+		if (strValue.equalsIgnoreCase("*")) {
+			query = "*";
+		} else {
+			String tempVal = "";
+			if (strValue.startsWith("*")) {
+				tempVal = strValue.substring(1) + "%";
+			}
+			if (strValue.endsWith("*")) {
+				if (!DataBaseIdentityUtil.isEmpty(tempVal)) {
+					tempVal = strValue;
+				} else {
+					tempVal = strValue.substring(0, strValue.length() - 1) + "%";
+				}
+			} else {
+				tempVal = strValue;
+			}
+			query = "T." + name + " LIKE '" + tempVal + "'";
+		}
+		logger.ok(operation + " Final Filter " + query);
+		logger.ok(operation + " Ended ");
+		return query;
+	}
+
+	@Override
+	protected String createGreaterThanExpression(GreaterThanFilter filter, boolean not) {
+		String operation = "createGreaterThanOrEqualExpression - ";
+		logger.ok(operation + " Started");
+		String query = null;
+		if (not) {
+			return query;
+		}
+
+		Attribute attr = filter.getAttribute();
+		if (attr == null || attr.getValue() == null || (attr.getValue() != null && attr.getValue().isEmpty())) {
+			return query;
+		}
+
+		String name = filter.getName();
+		Object val = attr.getValue().get(0);
+		if (DataBaseIdentityUtil.isEmpty(val)) {
+			throw new ConnectorException("The filter value can not be empty");
+		}
+		String strValue = DataBaseIdentityUtil.getString(val);
+
+		if (name.equalsIgnoreCase("updateDate")) {
+			query = "T." + name + " > FUNC('TO_DATE','"
+					+ DataBaseIdentityUtil.convertDateToString(new Date(new Long(strValue).longValue())) + "','"
+					+ DataBaseIdentityConstants.DB_DATE_FORMAT + "')";
+		} else {
+			query = "T." + name + " > '" + strValue + "'";
+		}
+
+		logger.ok(operation + " Final Filter " + query);
+		logger.ok(operation + " Ended ");
+		return query;
+	}
+
+	@Override
+	protected String createAndExpression(String leftExpression, String rightExpression) {
+		return leftExpression + " AND " + rightExpression;
+
+	}
+
+}
---
+++ src/main/java/identity/dbconnector/dao/UserProfileDao.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package identity.dbconnector.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import identity.dbconnector.domain.UserProfile;
+
+/**
+ * @author Hari Kiran
+ *
+ */
+public interface UserProfileDao {
+
+	public UserProfile findByLogin(String loginId);
+
+	public UserProfile findById(long id);
+
+	public UserProfile createUser(UserProfile userProfile);
+
+	public UserProfile updateUser(UserProfile userProfile);
+
+	public UserProfile deleteUser(long id);
+
+	public List<UserProfile> getAllUser();
+
+	public List<UserProfile> getUsersByChange(Date date);
+
+	public List<UserProfile> findByUsersCriteria(String criteria);
+
+	public void test();
+
+}
